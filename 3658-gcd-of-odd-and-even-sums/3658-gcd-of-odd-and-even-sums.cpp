cb25329420ee437eabf03600ce4acd73586f20b6
Mayank Sinha <154017022+maynksinha@users.noreply.github.com>
2026-07-19 02:45:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (10.19%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0, sumEven = 0;
+
+        for (int i = 1; i <= 2 * n; i++) {
+            if (i % 2 == 0)
+                sumEven += i;
+            else
+                sumOdd += i;
+        }
+
+        return __gcd(sumOdd, sumEven);
+    }
+};
